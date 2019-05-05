dfc034317766be8d8fa0da1e646fbda081a7767f
edf42001 <s011291@students.lmsd.org>
2019-05-05 11:56:09 -0400

    Use Encoder library that allows for two encoders at once

--- SeniorProject.ino
+++ SeniorProject.ino
@@ -1,28 +1,33 @@
-const int trackEncoderA = 2;
-const int trackEncoderB = 3;
-volatile long trackEncoder = 0;
+#include <Encoder.h>
+
+const int trackEncA = 2; //pins two and 3 have interupts. Use one per encoder
+const int trackEncB = 4;
+
+const int armEncA = 3;
+const int armEncB = 5;
+
+Encoder trackEnc(trackEncA, trackEncB);
+Encoder armEnc(armEncA, armEncB);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trackEncoderA, INPUT);
-  pinMode(trackEncoderB, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(trackEncoderA), trackEncoderPinChangeA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(trackEncoderB), trackEncoderPinChangeB, CHANGE);
-
   Serial.println("Hello");
+  
 }
+
+long trackEncValue = 0;
+long armEncValue = 0;
+long startTime = millis();
 
 void loop() {
-
-
-}
-
-void trackEncoderPinChangeA(){
-  trackEncoder += digitalRead(trackEncoderA) == digitalRead(trackEncoderB) ? -1:1;
-  Serial.println(trackEncoder);
-}
-
-void trackEncoderPinChangeB(){
-  trackEncoder += digitalRead(trackEncoderA) != digitalRead(trackEncoderB) ? -1:1;
+  long newTrackEncValue = trackEnc.read();
+  if(newTrackEncValue != trackEncValue){
+    trackEncValue = newTrackEncValue;
+    Serial.println(trackEncValue);
+  }
+  long newArmEncValue = armEnc.read();
+  if(newArmEncValue != armEncValue){
+    armEncValue = newArmEncValue;
+    Serial.println(armEncValue);
+  }
 }
