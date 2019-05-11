aa9603703335336be49d46fb3c4be22d63224499
edf42001 <s011291@students.lmsd.org>
2019-05-11 12:40:01 -0400

    Add observer to simulation

--- InvertedPendulumSimulation.m
+++ InvertedPendulumSimulation.m
@@ -38,24 +38,52 @@ Dc = [D];
 
 Cn = [1, 0, 0, 0];
 sys_ss = ss(A, B, Cn, 0);
-Nbar = rscale(sys_ss, K)
+Nbar = rscale(sys_ss, K);
 
 states = {'x', 'x_dot', 'phi', 'phi_dot'};
 inputs = {'r'};
 outputs = {'x', 'phi'};
 
-#sys_ss = ss(A, B, C, D, 'statename', states, 'inputname', inputs, 'outputname', outputs);
+sys_ss = ss(A, B, C, D, 'statename', states, 'inputname', inputs, 'outputname', outputs);
 sys_cl = ss(Ac, Bc * Nbar, Cc, Dc, 'statename', states, 'inputname', inputs, 'outputname', outputs);
 
 poles = eig(Ac);
+P = [-40, -41, -42, -43];
+
+L = place(A', C', P)';
 
 #co = ctrb(sys_ss);
 #controllability = rank(co);
+#ob = obsv(sys_ss);
+#observability = rank(ob)
 
-t= 0:0.01:5;
-r = 0.2 * ones(size(t));
-[y, t, x] = lsim(sys_cl, r, t);
-[AX, H1, H2] = plotyy(t, y(:,1),t,y(:,2),'plot');
-set(get(AX(1), 'Ylabel'), 'String', 'cart position (m)')
-set(get(AX(2), 'XLabel'), 'String', 'pendulum angle (rad)')
-title('Step Response with LQR control')
+#t= 0:0.01:5;
+#r = 0.2 * ones(size(t));
+#[y, t, x] = lsim(sys_cl, r, t);
+#[AX, H1, H2] = plotyy(t, y(:,1),t,y(:,2),'plot');
+#set(get(AX(1), 'Ylabel'), 'String', 'cart position (m)')
+#set(get(AX(2), 'XLabel'), 'String', 'pendulum angle (rad)')
+#title('Step Response with LQR control')
+Bd = B;
+Bd(2,1) = 1.7
+
+Ace = [(A-B*K) (B*K);
+       zeros(size(A)) (A-L*C)];
+Bce = [Bd*Nbar;
+       zeros(size(B))];
+Cce = [Cc zeros(size(Cc))];
+Dce = [0;0];
+
+states = {'x' 'x_dot' 'phi' 'phi_dot' 'e1' 'e2' 'e3' 'e4'};
+inputs = {'r'};
+outputs = {'x'; 'phi'};
+
+sys_est_cl = ss(Ace,Bce,Cce,Dce,'statename',states,'inputname',inputs,'outputname',outputs);
+
+t = 0:0.01:5;
+r = 0.2*ones(size(t));
+[y,t,x]=lsim(sys_est_cl,r,t);
+[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+set(get(AX(1),'Ylabel'),'String','cart position (m)')
+set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
+title('Step Response with Observer-Based State-Feedback Control')
